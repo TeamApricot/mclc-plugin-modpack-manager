633c5a0a36271e5058418c610307ba08c76fdd5b
TeamApricot <teamapricot@mail.ru>
2020-03-23 17:31:18 +0400

    Saving the code for future changes

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ class ModPackModule {
   }
 
   /**
+   * Shortcut function to move files
+   * @param {String} old_path
+   * @param {String} new_path
+   */
+  move(old_path, new_path){
+    const e = this._mclcInstance;
+    fs.rename(old_path, new_path, function (err) {
+      if (err) e.emit('debug', `[MCLC/${this.getName()}]: Error: ${err}`);
+    });
+  }
+
+  /**
    * Function call by MCLC to launch custom download process.
    * There the module download forge mods from curseforge with specified configuration.
    * @param {Object} handler
@@ -41,83 +53,125 @@ class ModPackModule {
   async download(handler) {
     return new Promise(async (resolve, reject) => {
       // There is a configuration for the module ?
-      if (this._options.hasOwnProperty('modpackManager') && this._options.modpackManager.hasOwnProperty('modslist')) {
-        this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking for mods folder`);
+      console.log(this._options.hasOwnProperty('modpackManager'), this._options.modpackManager.hasOwnProperty('modslist'));
+      if (this._options.hasOwnProperty('modpackManager')) {
 
-        // Getting the mods folder path
-        const modFolder = path.resolve(this._options.root, "mods")
-
-        // Is the mods folder exist ?
-        if (!fs.existsSync(modFolder)) {
-          // Creating recursively the mods folder
-          shelljs.mkdir('-p', modFolder);
-        }
-
-        // Getting all files available in the mod folder
-        let installedMods = fs.readdirSync(modFolder);
-
-        let modsList = [];
-
-        // Process all mods from the configuration
-        for (const projectID of Object.keys(this._options.modpackManager.modslist)) {
-          // Get the configuration for the current key
-          const modConfig = this._options.modpackManager.modsList[projectID];
-
+        if (this._options.modpackManager.hasOwnProperty('modpackId')){
           // Get the mod information from curseforge with the project ID
-          const {gameVersionLatestFiles: mods} = await twitchAppApi.getAddonInfo(projectID);
+          const {latestFiles: modpack} = await twitchAppApi.getAddonInfo(this._options.modpackManager.modpackId);
 
-          // Find the corresponding file version which match with the version ID specified in the configuration
-          const mod = mods.find((mods) => {
-            return mods.projectFileId === modConfig.fileId
-          })
+          console.log(modpack);
+          // this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloading ${info.fileName}`);
+          // await handler.downloadAsync(info.downloadUrl, modFolder, info.fileName, true, "mod")
+          //   .catch((error) => {
+          //     reject(error)
+          //   });
+          // this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloaded ${info.fileName}`);
+        }
+        else if (this._options.modpackManager.hasOwnProperty('modsList')){
+          this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking for mods folder`);
 
-          // Get the file information from curseforge with the project and file IDs
-          const info = await twitchAppApi.getAddonFileInformation(projectID, mod.projectFileId);
+          // Getting the mods folder path
+          const modFolder = path.resolve(this._options.root, "mods")
 
-          const fileExist = installedMods.find((name) => {
-            return name === info.fileName;
-          })
-
-          // Is the file already exist in the mods folder ?
-          if (fileExist) {
-            this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking checksum for file: ${info.fileName}`);
-            // Is the hash don't correspond for the current file ?
-            if (!await handler.checkSum(modConfig.hash, path.resolve(modFolder, info.fileName))) {
-              this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checksum invalid for file: ${info.fileName}`);
-              // Delete the file and remove it from the installed mods list
-              fs.unlinkSync(path.resolve(modFolder, info.fileName));
-              const index = installedMods.indexOf(info.fileName);
-              installedMods.splice(index, 1);
-            } else {
-              // The file is installed, just remove it and continue to the next iteration
-              const index = installedMods.indexOf(info.fileName);
-              installedMods.splice(index, 1);
-
-              continue;
-            }
+          // Is the mods folder exist ?
+          if (!fs.existsSync(modFolder)) {
+            // Creating recursively the mods folder
+            shelljs.mkdir('-p', modFolder);
           }
-          // Add the current mod to the mods list
-          modsList.push(info);
-        }
 
-        // Loop the mods list mod by mod
-        for (const info of modsList) {
-          // Download the mod file from curseforge
-          this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloading ${info.fileName}`);
-          await handler.downloadAsync(info.downloadUrl, modFolder, info.fileName, true, "mod")
-            .catch((error) => {
-              reject(error)
+          // Getting the unused mods folder path
+          const unusedModFolder = path.resolve(this._options.root, ".unused_mods")
+
+          // Is the unused mods folder exist ?
+          if (!fs.existsSync(unusedModFolder)) {
+            // Creating recursively the unused mods folder
+            shelljs.mkdir('-p', unusedModFolder);
+          }
+
+          // Getting all files available in the mod folder
+          let installedMods = fs.readdirSync(modFolder);
+          let installedUnusedMods = fs.readdirSync(unusedModFolder);
+
+          installedMods = installedMods.concat(installedUnusedMods);
+
+          let modsList = [];
+
+          // Process all mods from the configuration
+          for (const projectID of Object.keys(this._options.modpackManager.modsList)) {
+            // Get the configuration for the current key
+            const modConfig = this._options.modpackManager.modsList[projectID];
+
+            // Get the mod information from curseforge with the project ID
+            const {gameVersionLatestFiles: mods} = await twitchAppApi.getAddonInfo(projectID);
+
+            // Find the corresponding file version which match with the version ID specified in the configuration
+            const mod = mods.find((mods) => {
+              return mods.projectFileId === modConfig.fileId
+            })
+
+            // Get the file information from curseforge with the project and file IDs
+            const info = await twitchAppApi.getAddonFileInformation(projectID, mod.projectFileId);
+
+            const fileExist = installedMods.find((name) => {
+              return name === info.fileName;
+            })
+
+            const unusedFileExist = installedUnusedMods.find((name) => {
+              return name === info.fileName;
             });
-          this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloaded ${info.fileName}`);
-        }
 
-        this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking for forbidden files`);
+            // Is the file already exist in the mods folder ?
+            if (fileExist || unusedFileExist) {
+              console.log(info.fileName);
 
-        // Loop the latest entries available in the files list
-        for (const file of installedMods) {
-          // Delete the file
-          this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Delete forbidden file: ${file}`);
-          fs.unlinkSync(path.resolve(modFolder, file));
+              // If the mod is on unused folder than move it to the main one
+              if(unusedFileExist)
+                this.move(path.resolve(unusedModFolder, info.fileName), path.resolve(modFolder, info.fileName));
+
+              this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking checksum for file: ${info.fileName}`);
+              // Is the hash don't correspond for the current file ?
+              if (!await handler.checkSum(modConfig.hash, path.resolve(modFolder, info.fileName), {
+                algorithm: "md5"
+              })) {
+                this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checksum invalid for file: ${info.fileName}`);
+                // Delete the file and remove it from the installed mods list
+                fs.unlinkSync(path.resolve(modFolder, info.fileName));
+                const index = installedMods.indexOf(info.fileName);
+                installedMods.splice(index, 1);
+              }
+              else {
+                // The file is installed, just remove it and continue to the next iteration
+                const index = installedMods.indexOf(info.fileName);
+                installedMods.splice(index, 1);
+
+                continue;
+              }
+            }
+            // Add the current mod to the mods list
+            modsList.push(info);
+          }
+
+          // Loop the mods list mod by mod
+          for (const info of modsList) {
+            // Download the mod file from curseforge
+            this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloading ${info.fileName}`);
+            await handler.downloadAsync(info.downloadUrl, modFolder, info.fileName, true, "mod")
+              .catch((error) => {
+                reject(error)
+              });
+            this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Downloaded ${info.fileName}`);
+          }
+
+          this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Checking for forbidden files`);
+
+          // Loop the latest entries available in the files list
+          for (const file of installedMods) {
+            // Move the file
+            this._mclcInstance.emit('debug', `[MCLC/${this.getName()}]: Moving forbidden file: ${file}`);
+            this.move(path.resolve(modFolder, file), path.resolve(unusedModFolder, file));
+          }
+
         }
       }
 
